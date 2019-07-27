30b83310e9f24edc3b904376bed662905f0a1b20
Godsgift ThankGod Akari <giftakari@yahoo.com>
2019-07-27 19:21:04 +1000

    prototype

--- Constructor-Function/Constructor-Func.js
+++ Constructor-Function/Constructor-Func.js
@@ -1,3 +1,4 @@
+/***      Function  =>  This keyword */
 function User(firstName, lastName, age, gender) {
   this.firstName = firstName;
   this.lastName = lastName;
@@ -5,8 +6,15 @@ function User(firstName, lastName, age, gender) {
   this.gender = gender;
 }
 
+/** Instance of the object */
 let user1 = new User("Sammy", "Akari", 20, "Male");
 let user2 = new User("Pee", "Akari", 33, "Female");
+
+/** Prototypal  */
+User.prototype.emailDomain = "@akaris.com";
+User.prototype.getEmailAddress = function() {
+  return this.firstName + this.lastName + this.emailDomain;
+};
 
 console.log(user1);
 console.log(user2);
